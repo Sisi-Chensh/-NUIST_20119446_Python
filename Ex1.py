39cedb749fb4d55e6cc77fb21b210a5841663cff
3313409739@qq.com <bugs@gmail.com>
2026-03-16 22:48:33 +0800

    Add Ex1.py,Ex2.py,Ex3.py - Python practice files

---
+++ Ex1.py
@@ -0,0 +1,3 @@
+num = float(input('Enter a number'))
+sum = num*2
+print('The double of {0} is {1}'.format(num,sum))
---
+++ Ex3.py
@@ -0,0 +1,9 @@
+import random
+x = random.randint(10,20)
+print(x)
+found = 0
+while found = 0:
+guess=int(input('Guess a Number:'))
+if guess == x:
+found=1
+print('Congratulations!')
