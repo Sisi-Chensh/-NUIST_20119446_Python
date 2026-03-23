7d34566e3e2fad5931cfb4ae4e8a382cd6c224cd
3313409739@qq.com <bugs@gmail.com>
2026-03-23 08:43:08 +0800

    Add Version 1: AddTwoNums.py

---
+++ Ex4.py
@@ -0,0 +1,7 @@
+age = int(input('Please enter your age:'))
+if age <= 19:
+print('You are qualify for student discounts.')
+if 20 <= age <= 54:
+print('You qualify for no age discounts.')
+if age >= 55:
+print('You can receive senior discounts.')
